fe75162a2e8b6312c2a01d417fadd090259e4c53
Tahazib Santo <tahazibrahman21@gmail.com>
2025-08-15 22:41:29 +0000

    https://youtu.be/GVkjiC2seJA?si=qzmTBRZXniyP3BWK https://youtu.be/dpPbHI

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -6,7 +6,7 @@ import { Card, CardContent } from '@/components/ui/card';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { PlayCircle } from 'lucide-react';
 import type { Video } from '@/lib/types';
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious, type CarouselApi } from "@/components/ui/carousel";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious, type CarouselApi } from "@/components/carousel";
 
 const portfolioVideos: Video[] = [
   {
@@ -73,13 +73,53 @@ const portfolioVideos: Video[] = [
     youtubeUrl: 'https://www.youtube.com/embed/BHlspQuBZxc',
     aiHint: 'dramatic scene',
   },
-    {
+  {
     id: 'doWSF05xN80',
     title: 'Commercial Ad',
     description: 'A commercial advertisement.',
     thumbnailUrl: 'https://img.youtube.com/vi/doWSF05xN80/hqdefault.jpg',
     youtubeUrl: 'https://www.youtube.com/embed/doWSF05xN80',
     aiHint: 'product shot',
+  },
+  {
+    id: 'GVkjiC2seJA',
+    title: 'Cinematic Video',
+    description: 'A cinematic video.',
+    thumbnailUrl: 'https://img.youtube.com/vi/GVkjiC2seJA/hqdefault.jpg',
+    youtubeUrl: 'https://www.youtube.com/embed/GVkjiC2seJA',
+    aiHint: 'cinematic travel',
+  },
+  {
+    id: 'dpPbHISVOmY',
+    title: 'Travel Vlog',
+    description: 'A travel vlog.',
+    thumbnailUrl: 'https://img.youtube.com/vi/dpPbHISVOmY/hqdefault.jpg',
+    youtubeUrl: 'https://www.youtube.com/embed/dpPbHISVOmY',
+    aiHint: 'travel scenery',
+  },
+  {
+    id: 'gnWxnusmPuY',
+    title: 'Short Film Scene',
+    description: 'A scene from a short film.',
+    thumbnailUrl: 'https://img.youtube.com/vi/gnWxnusmPuY/hqdefault.jpg',
+    youtubeUrl: 'https://www.youtube.com/embed/gnWxnusmPuY',
+    aiHint: 'film noir',
+  },
+  {
+    id: 'w2Gd08E7Ch0',
+    title: 'Promotional Video',
+    description: 'A promotional video for a product.',
+    thumbnailUrl: 'https://img.youtube.com/vi/w2Gd08E7Ch0/hqdefault.jpg',
+    youtubeUrl: 'https://www.youtube.com/embed/w2Gd08E7Ch0',
+    aiHint: 'product commercial',
+  },
+  {
+    id: 'ltftA-IF3uA',
+    title: 'Music Video Clip',
+    description: 'A clip from a music video.',
+    thumbnailUrl: 'https://img.youtube.com/vi/ltftA-IF3uA/hqdefault.jpg',
+    youtubeUrl: 'https://www.youtube.com/embed/ltftA-IF3uA',
+    aiHint: 'music performance',
   }
 ];
 
@@ -124,9 +164,9 @@ export default function Portfolio() {
         </p>
       </div>
       <Carousel setApi={setApi} className="w-full" opts={{align: "center", loop: true}}>
-        <CarouselContent>
+        <CarouselContent className="-ml-2">
            {portfolioVideos.map((video, index) => (
-            <CarouselItem key={video.id} className="md:basis-1/3 lg:basis-1/5">
+            <CarouselItem key={video.id} className="md:basis-1/3 lg:basis-1/5 pl-2">
                <div
                 onClick={() => openVideoPlayer(video)}
                 className="p-1"
